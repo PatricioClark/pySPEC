6a260c64e183c5adbd9f12d1c6214b93295c35a8
Joaquin Cullen <joaquincullen@gmail.com>
2024-11-24 17:08:13 -0300

    Added newton solver for UPOs, tested with Kolmogorov flow run with python.

---
+++ pySPEC/newton/__init__.py
@@ -0,0 +1,4 @@
+from .newton import NewtonSolver
+from .upos import UPO
+from .gmres import GMRES, backsub
+
--- pySPEC/newton/newton.py
+++ pySPEC/newton/newton.py
@@ -9,51 +9,68 @@ from .. import pseudo as ps
 
 class NewtonSolver(abc.ABC):
 
-    def __init__(self, pm, solver):
+    def __init__(self, pmN, solver):
         ''' Initializes the Newton solver
 
         Parameters:
         ----------
-            pm: parameter dictionary
+            pmN: Newton parameters dictionary
             solver: solver object
         '''
-        self.pm = pm
+        self.pmN = pmN
         self.solver = solver
-        self.grid = solver.grid
+        self.grid = solver.grid # Podría no usar un grid
 
-    def iterate(self, fields, trust_region='hook'):
-        for i_newt in range(self.pm.restart+1, self.pm.N_newt):    
+    # @abc.abstractmethod
+    # def F(self, X):
+    #     pass
 
-            # Write to txts
-            # mod.write_prints(i_newt, b_norm, X, sx, T)
-            
-            # Calculate A matrix for newton iteration
-            apply_A = mod.application_function(solver.evolve,
-                                               inf_trans,
-                                               translation,
-                                               pm, X, T, Y, sx,
-                                               i_newt)
-            self.update_A()
-
-            # Perform GMRes iteration with A and RHS b
-            H, beta, Q, k = GMRES(self.apply_A, b, i_newt, self.pm)
-
-            if trust_region == 'hook':
-                X, Y, sx, T, b = hookstep(H, beta, Q, k, X, sx, T, b, i_newt)
-            elif trust_region == 'linesearch':
-                X, Y, sx, T, b = linesearch(H, beta, Q, k, X, sx, T, b, i_newt)
-            else:
-                pass
-
-            b_norm = np.linalg.norm(b)
-
-            #For every newton step save fields at t and t+T
-            mod.save_X(X, f'{i_newt}', pm, grid)
-            mod.save_X(Y, f'{i_newt}_T', pm, grid)
-
-            if b_norm < self.pm.tol_newt:
-                break
+    # @abc.abstractmethod
+    # def jac(self, F, X):
+    #     pass
 
     @abc.abstractmethod
-    def update_A(self):
+    def flatten(self, fields):
         pass
+
+    @abc.abstractmethod
+    def unflatten(self, U):
+        pass
+
+    @abc.abstractmethod
+    def update_A(self, *args):
+        pass
+
+    @abc.abstractmethod
+    def iterate(self, X):
+        pass
+    
+    # def iterate(self, X):
+    #     for i_newt in range(self.pmN.restart+1, self.pmN.N_newt):    
+
+    #         # Write to txts
+    #         if self.pmN.verbose:
+    #             self.write_prints(i_newt, b_norm, U, sx, T)
+            
+    #         # Calculate A matrix for newton iteration
+    #         A = self.update_A(X)
+
+    #         # Perform GMRes iteration with A and RHS b
+    #         H, beta, Q, k = GMRES(self.apply_A, b, i_newt, self.pmN)
+
+    #         if trust_region == 'hook':
+    #             X, Y, sx, T, b = hookstep(H, beta, Q, k, X, sx, T, b, i_newt)
+    #         elif trust_region == 'linesearch':
+    #             X, Y, sx, T, b = linesearch(H, beta, Q, k, X, sx, T, b, i_newt)
+    #         else:
+    #             pass
+
+    #         b_norm = np.linalg.norm(b)
+
+    #         #For every newton step save fields at t and t+T
+    #         mod.save_X(X, f'{i_newt}', pmN, grid)
+    #         mod.save_X(Y, f'{i_newt}_T', pmN, grid)
+
+    #         if b_norm < self.pmN.tol_newt:
+    #             break
+
