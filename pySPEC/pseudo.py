4f355195e3e17e52e6faab6421acb001ffac037e
Joaquin Cullen <joaquincullen@gmail.com>
2024-11-24 16:59:27 -0300

    Fixed avg. Added enstrophy, translation.

--- pySPEC/pseudo.py
+++ pySPEC/pseudo.py
@@ -21,6 +21,9 @@ class Grid1D:
         self.kk = kk
         self.kr = ki
 
+        self.N = pm.Nx
+        self.shape = (pm.Nx,)
+
         # Norm and de-aliasing
         self.norm = 1.0/(pm.Nx**2)
         self.zero_mode = 0
@@ -53,10 +56,36 @@ class Grid1D:
             prod += (ca*cb.conjugate()).real
         return prod
 
+    def avg(self, ui):
+        ''' Mean in Fourier space '''
+        ui[:,1:] *= 2 # Factor 2 for negative frequencies
+        return self.norm * np.sum(ui)
+
     def energy(self, fields):
         u2  = self.inner(fields, fields)
         eng = 0.5*self.avg(u2)
         return eng
+
+    def enstrophy(self, fields):
+        u2  = self.inner(fields, fields)
+        ens = 0.5*self.avg(self.k2*u2)
+        return ens
+
+    def translate(self, fields, sx):
+        # Forward transform
+        f = [self.forward(ff) for ff in fields]
+        # Translate
+        f = [ff*np.exp(1.0j*self.kx*sx) for ff in f]
+        # Inverse transform
+        fields = [self.inverse(ff) for ff in f]
+        return fields
+
+    def deriv_fields(self, fields):
+        ''' Compute derivatives in Fourier space '''
+        f = [self.forward(ff) for ff in fields]
+        f = [self.deriv(ff, self.kx) for ff in f]
+        return [self.inverse(ff) for ff in f]
+
 
 class Grid2D(Grid1D):
     def __init__(self, pm):
@@ -89,6 +118,10 @@ class Grid2D(Grid1D):
         self.kk = kk
         self.kr = kr
 
+        self.N = pm.Nx*pm.Ny
+        self.shape = (pm.Nx, pm.Ny)
+
+
         # Norm, de-aliasing and solenoidal mode proyector
         self.norm = 1.0/(pm.Nx**2*pm.Ny**2)
         self.zero_mode = (0, 0)
@@ -98,6 +131,16 @@ class Grid2D(Grid1D):
             self.pxx = np.nan_to_num(1.0 - self.kx**2/k2)
             self.pyy = np.nan_to_num(1.0 - self.ky**2/k2)
             self.pxy = np.nan_to_num(- self.kx*self.ky/k2)
+
+    def translate2D(self, fields, sx, sy):
+        # Forward transform
+        f = [self.forward(ff) for ff in fields]
+        # Translate
+        f = [ff *np.exp(1.0j*self.kx*sx) *np.exp(1.0j*self.ky*sy) for ff in f]
+        # Inverse transform
+        fields = [self.inverse(ff) for ff in f]
+        return fields
+
 
     def inc_proj(self, fields):
         ''' Project onto solenoidal modes '''
