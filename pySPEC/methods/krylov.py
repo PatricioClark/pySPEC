Fixed dtype of created variables (single vs double precision) in krylov.py

pySPEC/methods/krylov.py
@@ -29,7 +29,7 @@ def GMRES(A, b, N_gmres, tol_gmres, iN:int|None = None, glob_method = 1, iA:int|
     n = N_gmres
 
     # Determine the data type based on b
-    dtype = np.complex_ if np.iscomplexobj(b) else np.float64
+    dtype = b.dtype
 
     #Initialize sine and cosine Givens 1d vectors. This allows the algorithm to be O(k) instead of O(k^2)
     sn = np.zeros(n,dtype=dtype)
@@ -91,7 +91,7 @@ def arnoldi_step(A, Q, k):
         v = A @ Q[:, k-1]  # generate candidate vector using matrix multiplication
 
     # Determine the data type based on Q and v
-    dtype = np.complex_ if (np.iscomplexobj(Q) or np.iscomplexobj(v)) else np.float64
+    dtype = v.dtype
 
     h = np.zeros(k+1, dtype=dtype)
 
@@ -125,7 +125,7 @@ def backsub(R, b):
     n = len(b)
 
     # Determine the data type based on b
-    dtype = np.complex_ if np.iscomplexobj(b) else np.float64
+    dtype = b.dtype
 
     x = np.zeros(n,dtype=dtype)
     
@@ -142,7 +142,7 @@ def arnoldi_eig(A, b, n, tol):
     """
 
     # Determine the data type based on b
-    dtype = np.complex_ if np.iscomplexobj(b) else np.float64
+    dtype = b.dtype
 
     #Unitary base of Krylov subspace (maximum number of cols: n)
     Q = np.zeros((len(b), n), dtype=dtype)